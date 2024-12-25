ce292725d2bdc6e85620c377b2dacdcc00eab4d6
Sandip Chaudhari <sandipchaudhari@Sandips-MacBook-Air.local>
2024-12-25 09:23:29 +0530

    Add : 1. enable cors 2. added body parser

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'], // Enable all log levels
   });
+  // Enable CORS
+  app.enableCors({
+    origin: '*', // Allow all origins (customize as needed)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+  // Use body-parser middleware
+  app.use(bodyParser.json({ limit: '10mb' })); // Parse JSON requests up to 10MB
+  app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' })); // Parse URL-encoded requests
 
   // Swagger configuration
   const config = new DocumentBuilder()
